c46da3e74b27155bbce565c546e6a79b8af2920f
YihuaPu <44237498+YihuaPu@users.noreply.github.com>
2023-01-31 19:03:45 -0500

    jkh
    2

--- exercises/exercise02/Assets/PlayerController.cs
+++ exercises/exercise02/Assets/PlayerController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0.001f;
-    public float lauchForce = 9999;
+    public float lauchForce = 100;
 
-    float rotateSpeed = 15f;
+    float rotateSpeed = 0.1f;
 
     public Rigidbody rb;
+
+    float lastRotateDirectionSwitchTime = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // if (Time.time - lastRotateDirectionSwitchTime > 1f)
+        // {
+        //     rotateSpeed = rotateSpeed * -1;
+        //     lastRotateDirectionSwitchTime = Time.time;
+        // }
+        // gameObject.transform.Rotate(0, rotateSpeed, 0);
+
         // Vector3 newPosition = new Vector3(gameObject.transform.position.x + 0.05f, gameObject.transform.position.y, gameObject.transform.position.z);
         // gameObject.transform.position = newPosition;
             if (Input.GetKey(KeyCode.Space))
@@ -28,5 +37,17 @@ public class PlayerController : MonoBehaviour
             }
         
 
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "TargetCube")
+        {
+            col.gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+            // If you wanted to remove the cube rather than change its color, you
+            // would "Destroy" it using the following line of code.
+            //Destroy(col.gameObject);
+        }
     }
 }
